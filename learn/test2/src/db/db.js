e72ac4f903a5e944fd5ec4da1f0dc3919c026198
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-07-14 03:02:22 +0530

    adding dotenv

--- learn/test2/src/db/db.js
+++ learn/test2/src/db/db.js
@@ -4,11 +4,9 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const mongoose=require('mongoose')
 async function connectdb() {
-    await mongoose.connect("mongodb+srv://backend:FoFrI9kZtuFOlNHR@backend.tksmref.mongodb.net/test2db")
+    await mongoose.connect(process.env.MONOGO_URI)
     console.log("connected to DB")
 }
 
 module.exports=connectdb
 
-//mongodb+srv://backend:<db_password>@backend.tksmref.mongodb.net/
-// mongodb+srv://backend:FoFrI9kZtuFOlNHR@backend.tksmref.mongodb.net/
