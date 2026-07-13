926dcdd68b6a311e3ecaad243579a1a47aafc971
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-07-14 01:36:44 +0530

    basics->test0

---
+++ learn/test0/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+
+
+const app=express()  // creating server instance
+
+app.get("/",(req, res) => {
+    res.send("hlw world");
+})
+
+app.get("/about",(req,res)=>{
+    res.send("about page");
+})
+
+app.listen(3000, () => {
+    console.log("server running on port 3000")
+})  //start server
+
